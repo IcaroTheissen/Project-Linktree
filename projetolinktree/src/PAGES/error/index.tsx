1a82a182b5e5b721bebbebd0b058a04689af4c24
Ícaro Theissen <icaro.theissen@hotmail.com>
2025-01-08 21:32:02 -0300

    Quinta atualização do Projeto
    Quinta atualização do Projeto

--- projetolinktree/src/PAGES/error/index.tsx
+++ projetolinktree/src/PAGES/error/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function ErrorPage(){
     return(
         <div className="flex w-full min-h-screen justify-center items-center flex-col text-white">
-            <h1 className="font-bold text-4xl">Error 404</h1>
+            <h1 className="font-bold text-4xl">Error 404 ‼️</h1>
             <h1 className="font-bold text-4xl mb-4">Página não encontrada</h1>
             <p className="italic text-2xl mb-2">Está página não existe</p>
 
